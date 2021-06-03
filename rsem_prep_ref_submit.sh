35f0b5eb749966d6cea477a0de64a27b14e7324c
kellywithsword <kdeweese@usc.edu>
2021-06-03 15:20:11 -0700

    Added RSEM scripts

---
+++ rsem_prep_ref_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p cegs
+#SBATCH --mem=30gb
+#SBATCH --time=30:00:00
+#SBATCH -t 12
+
+source ~/bin/anaconda3/etc/profile.d/conda.sh
+conda activate rsem
+
+reference=$1
+reference_no_ext=`echo "$reference" | sed 's/\..*//g'`
+sample_id=$2
+echo "Aligning sample $sample_id to $reference_no_ext"
+
+rsem-calculate-expression --bowtie2 --sort-bam-by-coordinate -p 12 --paired-end ${sample_id}_R1.fastq.gz ${sample_id}_R2.fastq.gz $reference_no_ext $sample_id
+
---
+++ rsem_submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p cegs
+#SBATCH --mem=20gb
+#SBATCH --time=03:00:00
+
+source ~/bin/anaconda3/etc/profile.d/conda.sh
+conda activate rsem
+
+reference=$1
+reference_no_ext=`echo "$reference" | sed 's/\..*//g'`
+echo "Preparing reference $reference_no_ext"
+
+rsem-prepare-reference --bowtie2 $reference $reference_no_ext
+
+
